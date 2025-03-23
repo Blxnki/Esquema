bbac1577b0a1a98dafc9e954098e69b539f1689f
Blxnki <159130524+Blxnki@users.noreply.github.com>
2025-03-23 23:42:34 +0000

    Add files via upload

---
+++ Variables y constantes/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//Variable
+let nombre = "John";
+let edad = 30;
+let estaCasado = false;
+let altura = 1.80;
+let peso = 70.0;
+let a
+console.log(nombre, edad, estaCasado, altura, peso);
+console.log(a)
+//Constantes
+const PI = 3.1416;
+const RUTA = "/home/user";
+console.log(RUTA); 
+console.log(PI);
