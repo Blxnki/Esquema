fc1c068690e80b3621957a2ee572bd1b8fb9b360
Blxnki <159130524+Blxnki@users.noreply.github.com>
2025-03-23 23:43:03 +0000

    Add files via upload

---
+++ sintaxis básico/script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+//Esto es un comentario de una linea
+
+/*Esto es 
+un comentario para varias lineas*/
+
+//Con let se usa para declarar variables 
+let numero = 10;
+//Como puedes ver se pueden declarar varia variables en una sola linea
+let numero2 = 20, suma = numero + numero2;
+let boolean = false;
+let texto = "Hola mundo";
+console.log(numero);
+console.log(boolean);
+console.log(texto);
+console.log(suma);
+
+//Con const se usa para declarar variables que no cambian
+const nombre = "Juan";
+console.log(nombre);
