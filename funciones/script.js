6eee06db0a48974e6aaa2450d1dda1ce666e9aa2
Blxnki <159130524+Blxnki@users.noreply.github.com>
2025-03-23 23:43:47 +0000

    Add files via upload

---
+++ funciones/script.js
@@ -0,0 +1,31 @@
+"use strict"
+//funcion
+let numero = 0, numeroFactorial = 0;
+
+function factorial(x) {
+    return x < 2 ? 1 : x * factorial(x - 1);
+};
+
+numero = Number(prompt("Número:"));
+
+if (numero != NaN && numero == 0 && numero > 0) {
+    numeroFactorial = factorial(numero);
+    console.log("El factorial de " + numero + " es " + numeroFactorial);
+} else {
+    console.log("Error, introduzca un numero positivo y entero");
+};
+
+function sumar(...listaArgumentos) {
+    let suma = 0;
+    for (let argumento of listaArgumentos) suma += argumento;
+    return suma
+};
+
+let suma1 = sumar(1, 2, 3, 4, 5);
+let suma2 = sumar(10, 20);
+let suma3 = sumar(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+console.log("1+2+3+4+5 =" + suma1 + "\n"
+            + "10+20 =" + suma2 + "\n"
+            + "1+2+3+4+5+6+7+8+9 =" + suma3);
+
